0e7fbc1bc80fad7d3ce43ad291fce53ae2f2905b
Oleksandr-Stepanyshyn <88288677+Oleksandr-Stepanyshyn@users.noreply.github.com>
2022-04-25 12:03:24 +0300

    finish

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import { Gallery } from "./ImageGallery.styled"
+
+export const ImageGallery = ({children}) => {
+  return(
+    <Gallery>
+      {children}
+    </Gallery>
+  )
+}
+
+ImageGallery.prototype ={
+  children: PropTypes.any.isRequired,
+}
